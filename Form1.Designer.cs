2407b3053991c6ef3219f9803a8b57ecfdbcc028
Abdullatif <abdullatifalmhmd@gmail.com>
2026-07-14 03:16:43 +0300

    Final Timer

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Timer_Project
+{
+    partial class frmTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTimer));
+            this.progTimer = new System.Windows.Forms.ProgressBar();
+            this.lbPercentage = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.txtMinutes = new System.Windows.Forms.TextBox();
+            this.txtSeconds = new System.Windows.Forms.TextBox();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // progTimer
+            // 
+            this.progTimer.Location = new System.Drawing.Point(12, 12);
+            this.progTimer.Name = "progTimer";
+            this.progTimer.Size = new System.Drawing.Size(511, 20);
+            this.progTimer.Step = 1;
+            this.progTimer.TabIndex = 0;
+            // 
+            // lbPercentage
+            // 
+            this.lbPercentage.AutoSize = true;
+            this.lbPercentage.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPercentage.Location = new System.Drawing.Point(532, 12);
+            this.lbPercentage.Name = "lbPercentage";
+            this.lbPercentage.Size = new System.Drawing.Size(62, 20);
+            this.lbPercentage.TabIndex = 1;
+            this.lbPercentage.Text = "00.00%";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(422, 238);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(183, 50);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.TabStop = false;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(218, 238);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(183, 50);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.TabStop = false;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 238);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(183, 50);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.TabStop = false;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "h,";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(346, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 32);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "m,";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(491, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 32);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "s";
+            // 
+            // txtHours
+            // 
+            this.txtHours.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtHours.Font = new System.Drawing.Font("OCR A Extended", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHours.ForeColor = System.Drawing.Color.Black;
+            this.txtHours.Location = new System.Drawing.Point(114, 102);
+            this.txtHours.MaxLength = 2;
+            this.txtHours.Multiline = true;
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(81, 42);
+            this.txtHours.TabIndex = 0;
+            this.txtHours.Text = "00";
+            this.txtHours.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtHours.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtHours_KeyPress);
+            // 
+            // txtMinutes
+            // 
+            this.txtMinutes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMinutes.Font = new System.Drawing.Font("OCR A Extended", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMinutes.ForeColor = System.Drawing.Color.Black;
+            this.txtMinutes.Location = new System.Drawing.Point(259, 102);
+            this.txtMinutes.MaxLength = 2;
+            this.txtMinutes.Multiline = true;
+            this.txtMinutes.Name = "txtMinutes";
+            this.txtMinutes.Size = new System.Drawing.Size(81, 42);
+            this.txtMinutes.TabIndex = 1;
+            this.txtMinutes.Text = "00";
+            this.txtMinutes.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtMinutes.TextChanged += new System.EventHandler(this.txtHours_TextChanged);
+            this.txtMinutes.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtHours_KeyPress);
+            // 
+            // txtSeconds
+            // 
+            this.txtSeconds.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSeconds.Font = new System.Drawing.Font("OCR A Extended", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSeconds.ForeColor = System.Drawing.Color.Black;
+            this.txtSeconds.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtSeconds.Location = new System.Drawing.Point(404, 102);
+            this.txtSeconds.MaxLength = 2;
+            this.txtSeconds.Multiline = true;
+            this.txtSeconds.Name = "txtSeconds";
+            this.txtSeconds.Size = new System.Drawing.Size(81, 42);
+            this.txtSeconds.TabIndex = 2;
+            this.txtSeconds.Text = "00";
+            this.txtSeconds.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtSeconds.TextChanged += new System.EventHandler(this.txtHours_TextChanged);
+            this.txtSeconds.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtHours_KeyPress);
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // frmTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 300);
+            this.Controls.Add(this.txtSeconds);
+            this.Controls.Add(this.txtMinutes);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lbPercentage);
+            this.Controls.Add(this.progTimer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmTimer";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timer";
+            this.Load += new System.EventHandler(this.frmTimer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progTimer;
+        private System.Windows.Forms.Label lbPercentage;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.TextBox txtMinutes;
+        private System.Windows.Forms.TextBox txtSeconds;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timer_Project
+{
+    public partial class frmTimer : Form
+    {
+        int Hours = 0, Minutes = 0, Seconds = 0;
+        bool State = true;
+        TimeSpan timeRemainder;
+        double TimerInSeconds;
+        public frmTimer()
+        {
+            InitializeComponent();
+        }
+
+        private void ResetForm()
+        {
+            lbPercentage.Text = "0.00%";
+            txtHours.Text = "0";
+            txtMinutes.Text = "0";
+            txtSeconds.Text = "0";
+            btnStart.Enabled = true;
+            progTimer.Maximum = 0;
+            progTimer.Value = 0;
+            State = true;
+
+            txtHours.Enabled = true;
+            txtMinutes.Enabled = true;
+            txtSeconds.Enabled = true;
+
+            Timer.Enabled = false;
+
+            Hours = 0;
+            Minutes = 0;    
+            Seconds = 0;
+        }
+
+        private void frmTimer_Load(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+            PlayButtonSound();
+
+        }
+
+        private void txtHours_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (txtHours.Text == "0" && txtMinutes.Text == "0" && txtSeconds.Text == "0")
+            {
+                MessageBox.Show("You must do so before entering the time."
+                    , "Warning.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            PlayButtonSound();
+
+            Timer.Enabled = false;
+            btnPause.Enabled = false;
+            btnStart.Enabled = true;
+        }
+
+        private void txtHours_TextChanged(object sender, EventArgs e)
+        {
+            TextBox Txt= ((TextBox)sender);
+            if (String.IsNullOrEmpty(Txt.Text)) return;
+
+            if(int.TryParse(Txt.Text,out int Value))
+            {
+                if (Value > 60)
+                {
+                    Txt.Text = "60";
+                    Txt.SelectionStart = Txt.Text.Length;
+                }
+            }
+        }
+
+        private void PlayEndTimerSound()
+        {
+            SoundPlayer Sound = new SoundPlayer(Properties.Resources.TimerEnd_Sound);
+            Sound.Play();
+        }
+
+        private void PlayButtonSound()
+        {
+            SoundPlayer Sound = new SoundPlayer(Properties.Resources.OK_FINAL_SOUND);
+            Sound.Play();
+        }
+
+        private void SetStateIfTxtIsNull(TextBox txt)
+        {
+            if (String.IsNullOrWhiteSpace(txt.Text))
+            {
+                txt.Text = "0";
+            }
+        }
+
+        private void SetFirstTimerState()
+        {
+            Timer.Enabled = true;
+            btnPause.Enabled = true;
+
+            SetStateIfTxtIsNull(txtHours);
+            SetStateIfTxtIsNull(txtMinutes);
+            SetStateIfTxtIsNull(txtSeconds);
+
+            Hours = Convert.ToInt32(txtHours.Text);
+            Minutes = Convert.ToInt32(txtMinutes.Text);
+            Seconds = Convert.ToInt32(txtSeconds.Text);
+            btnStart.Enabled = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (txtHours.Text == "0" && txtMinutes.Text == "0" && txtSeconds.Text == "0")
+            {
+                MessageBox.Show("You must do so before entering the time."
+                    , "Warning.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; 
+            }
+            PlayButtonSound();
+
+            SetFirstTimerState();
+
+            txtHours.Enabled = false;
+            txtMinutes.Enabled = false;
+            txtSeconds.Enabled= false;
+        } 
+
+        private void CounterAndChangeTimer()
+        {
+            if (Seconds > 0)
+            {
+                Seconds--;
+                txtSeconds.Text = (Seconds).ToString();
+            }
+            else
+            {
+
+                if (Minutes > 0)
+                {
+                    Seconds = 59;
+                    Minutes--;
+                    txtMinutes.Text = (Minutes).ToString();
+                    txtSeconds.Text=(Seconds).ToString();
+                }
+                else if (Hours > 0)
+                {
+                    Seconds = 59;
+                    Minutes = 59;
+                    Hours--;
+                    txtHours.Text = (Hours).ToString();
+                    txtMinutes.Text = (Minutes).ToString();
+                    txtSeconds.Text = (Seconds).ToString();
+                }
+                else
+                {
+                    txtSeconds.Text = (Seconds).ToString();
+                    Timer.Enabled = false;
+
+                    if (txtHours.Text == "0" && txtMinutes.Text == "0" && txtSeconds.Text == "0")
+                        PlayEndTimerSound();
+                }
+            }
+        }
+
+        private void ChangeProgressParState(double ElapsedSeconds)
+        {
+            if(State)
+           {  
+                timeRemainder = new TimeSpan(Hours, Minutes, Seconds);
+
+                TimerInSeconds= timeRemainder.TotalSeconds;
+                progTimer.Maximum = Convert.ToInt32(TimerInSeconds);
+                State = false;
+            }
+
+            if (progTimer.Value < progTimer.Maximum)
+            {
+                progTimer.Value += 1; 
+
+            }
+
+            lbPercentage.Text = (((TimerInSeconds - ElapsedSeconds) / TimerInSeconds) * 100).ToString("F2") + "%";
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan ElapsedTime = new TimeSpan(Hours, Minutes, Seconds);
+            ChangeProgressParState(ElapsedTime.TotalSeconds);
+            CounterAndChangeTimer();
+        }
+    }
+}
